78c6b835733572c3410f0f294dfac69838235a4c
Marina <avesutok@gmail.com>
2018-09-13 08:40:39 +0300

    Added

---
+++ src/main/java/ru/otus/Main.java
@@ -0,0 +1,17 @@
+package ru.otus;
+
+import java.util.Arrays;
+import com.google.common.base.Joiner;
+
+public class Main {
+    public static void main(String args[]) {
+        Main example = new Main();
+        example.testExample();
+    }
+
+    private void testExample() {
+        System.out.println(Joiner.on(",")
+                .skipNulls()
+                .join(Arrays.asList(1,2,3,4,5,null,6)));
+    }
+}
