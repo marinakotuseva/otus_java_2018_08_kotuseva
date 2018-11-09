3ab333ffa37f3d407caed9a71c0af8e1a34dbee3
Marina <avesutok@gmail.com>
2018-11-10 01:26:25 +0300

    Fix

--- otus_6.1/src/main/java/ru/otus/CacheEngine.java
+++ otus_6.1/src/main/java/ru/otus/CacheEngine.java
@@ -1,14 +1,76 @@
 package ru.otus;
 
 import java.lang.ref.SoftReference;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class CacheEngine<K, V> {
     private final int maxElements;
     private final long lifeTimeMs;
 
-    private final Map<K, SoftReference<MapElement<K, V>>> elements = new LinkedHashMap<>();
+    private final Map<K, SoftReference<MapElement<K, V>>> elements = new Map<K, SoftReference<MapElement<K, V>>>() {
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public SoftReference<MapElement<K, V>> get(Object key) {
+            return null;
+        }
+
+        @Override
+        public SoftReference<MapElement<K, V>> put(K key, SoftReference<MapElement<K, V>> value) {
+            return null;
+        }
+
+        @Override
+        public SoftReference<MapElement<K, V>> remove(Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(Map<? extends K, ? extends SoftReference<MapElement<K, V>>> m) {
+
+        }
+
+        @Override
+        public void clear() {
+
+        }
+
+        @Override
+        public Set<K> keySet() {
+            return null;
+        }
+
+        @Override
+        public Collection<SoftReference<MapElement<K, V>>> values() {
+            return null;
+        }
+
+        @Override
+        public Set<Entry<K, SoftReference<MapElement<K, V>>>> entrySet() {
+            return null;
+        }
+    };
 
     CacheEngine(int m, long l) {
         this.maxElements = m;
@@ -27,12 +89,12 @@ public class CacheEngine<K, V> {
 
     }
 
-    public SoftReference<MapElement<K, V>> get(K key){
+    public V get(K key){
         SoftReference<MapElement<K, V>> element = elements.get(key);
         if (element == null) {
             return null;
         } else {
-            return element;
+            return element.get().getValue();
         }
     }
 
