a1d375e5dd76759f14200fb8e79dc73b0cad1994
Marina <avesutok@gmail.com>
2018-11-23 00:16:31 +0300

    Major fixes

--- otus_9.1/src/main/java/ru/otus/Main.java
+++ otus_9.1/src/main/java/ru/otus/Main.java
@@ -1,63 +1,126 @@
 package ru.otus;
 
-import java.io.FileNotFoundException;
+import com.google.gson.Gson;
+
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import org.json.simple.JSONObject;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         Person pers = new Person();
         pers.name = "Casey";
         pers.surname = "Smith";
         pers.age = 27;
         pers.hobby = "Saving the world";
-        String[][] ad = new String[2][2];
-        ad[0] = new String[]{"street", "BakerStreet"};
-        ad[1] = new String[]{"house", "221b"};
-        pers.address = ad;
         pers.bool = true;
+        pers.intNum = 3;
 
-        String res = "[{";
-        for (Field f:
-                pers.getClass().getDeclaredFields()) {
-            String fType = f.getType().getName();
-            String fName = f.getName();
-            System.out.println(fType);
-            switch (fType) {
-                case "java.lang.String":
-                case "java.lang.Integer":
-                case "boolean":
-                case "byte":
-                case "short":
-                case "char":
-                case "int":
-                case "long":
-                case "float":
-                case "double":
-                    try {
-                        Object fValue = f.get(pers);
-                        res += "\"" + fName + "\"" + ":" + "\"" + fValue+ "\"" + ",\n" ;
-                    } catch (Exception e){
-                        e.printStackTrace();
+        pers.address = new HashMap<>();
+        pers.address.put("street", "BakerStreet");
+        pers.address.put("house", "221b");
+
+        int[] arr = {2, 3};
+        pers.arr = arr;
+        pers.coll = new ArrayList();
+        pers.coll.add(0, "1");
+        pers.coll.add(0, "2");
+
+        String res = objectToJson(pers);
+        System.out.println("=======");
+        System.out.println("Custom: " + res);
+
+        Gson gson = new Gson();
+        System.out.println("Gson:   " + gson.toJson(pers));
+
+    }
+
+
+
+    public static String toJson(Object object) {
+        if (object == null){
+            return "null";
+        } else {
+            Class objectClass = object.getClass();
+            String res = "";
+            if (objectClass.isPrimitive()) {
+                if (object instanceof Integer || object instanceof Boolean) {
+                    res = object.toString();
+                } else {
+                    res = "\"" + object.toString() + "\"";
+                }
+            } else if (objectClass.isArray()) {
+                int length = Array.getLength(object);
+                String s ="[";
+                for (int i = 0; i < length; i++) {
+                    Object el = Array.get(object, i);
+                    s+=toJson(el);
+                    s+=",";
+                }
+                s = s.substring(0, s.length() - 1);
+                s+="]";
+                res = s;
+            } else if (objectClass.isAssignableFrom(Collection.class)) {
+                Collection objectToCollection = (Collection) object;
+                String s ="[";
+                for (Object o :
+                        objectToCollection) {
+                    s+=toJson(o);
+                    s+=",";
+                }
+                s = s.substring(0, s.length() - 1);
+                s+="]";
+                res = s;
+            } else if (objectClass.isAssignableFrom(HashMap.class)) {
+                Map<Object, Object> objectToMap = (Map) object;
+                res = "{";
+                for (var entry :
+                        objectToMap.entrySet()) {
+                    res += "\"" + entry.getKey() + "\"";
+                    res += ":";
+                    res += toJson(entry.getValue());
+                    res += ",";
+                }
+                res = res.substring(0, res.length() - 1);
+                res += "}";
+
+            } else {
+                if (object == null) {
+                    res = "\"" + null + "\"";
+                } else {
+                    if (object instanceof Integer || object instanceof Boolean) {
+                        res = object.toString();
+                    } else {
+                        res = "\"" + object.toString() + "\"";
                     }
-                    break;
-                case "[[Ljava.lang.String;":
-                    try {
-                        String[][] fValue = (String[][]) f.get(pers);
-                        res += "\"" + fName + "\"" + ":\n[{";
-                        for (String[] arr:
-                                fValue) {
-                            res+="\"" + arr[0] + "\":" + "\"" + arr[1] + "\"" + ",";
-                        }
-                        res = res.substring(0, res.length() - 1);
-                        res+="}]\n";
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    break;
+                }
             }
+            return res;
         }
-        res+="\n}]";
-        System.out.println(res);
+    }
+
+    public static String objectToJson(Object object){
+
+        StringBuilder res = new StringBuilder();
+        res.append("{");
+
+        for (Field f: object.getClass().getDeclaredFields()) {
+            String fName = f.getName();
+
+            try {
+                f.setAccessible(true);
+                Object fValue = f.get(object);
+                res.append("\"" + fName + "\"");
+                res.append(":");
+                res.append(toJson(fValue));
+            } catch (Exception e){
+                res.append("\"" + "error getting value" + "\"");
+                e.printStackTrace();
+            }
+            res.append(",");
+        }
+        res.deleteCharAt(res.lastIndexOf(","));
+        res.append("}");
+        return res.toString();
     }
 }
--- otus_9.1/src/main/java/ru/otus/Person.java
+++ otus_9.1/src/main/java/ru/otus/Person.java
@@ -1,12 +1,20 @@
 package ru.otus;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Person {
     String name;
     String surname;
     Integer age;
     String hobby;
-    String[][] address;
+    Map<String, String> address;
     boolean bool;
+    int intNum;
+    int[] arr;
+    ArrayList coll;
+
+
 }
