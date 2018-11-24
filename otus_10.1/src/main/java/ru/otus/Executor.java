38c3a419f2d755ae8543db6adb83e0784ca13ddc
Marina <avesutok@gmail.com>
2018-11-24 23:19:34 +0300

    First version

---
+++ otus_10.1/src/main/java/ru/otus/Executor.java
@@ -0,0 +1,24 @@
+package ru.otus;
+
+import java.sql.*;
+
+public class Executor {
+//    public void execNoReturn(Connection conn, String query){
+//    try {
+//        PreparedStatement s = conn.prepareStatement(query);
+//
+//        s.executeUpdate(query);
+//        s.
+//    } catch (SQLException e) {
+//        e.printStackTrace();
+//        try {
+//            conn.rollback();
+//        } catch (SQLException e1) {
+//            e1.printStackTrace();
+//        }
+//    }}
+
+
+}
+
+
---
+++ otus_10.1/src/main/java/ru/otus/Main.java
@@ -0,0 +1,55 @@
+package ru.otus;
+
+import org.reflections.Reflections;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException {
+        String drop = "drop table TestTable";
+        String createTable =  "create table TestTable " +
+                "(id bigint(20) not null auto_increment, " +
+                " name varchar(255), " +
+                " age int(3))";
+
+        Class.forName ("org.h2.Driver");
+        try {
+            Connection conn = DriverManager.getConnection ("jdbc:h2:~/test", "sa","1Qwerty");
+            Statement stmt = conn.createStatement();
+
+            stmt.executeUpdate(drop);
+            System.out.println("Table dropped");
+
+            stmt.executeUpdate(createTable);
+            System.out.println("Table created");
+            conn.commit();
+            conn.close();
+
+            UserDataSet user = new UserDataSet(1, "Jack", 10);
+            UserDataSet user2 = new UserDataSet(2, "Daniel", 13);
+
+            UserDataSet.save(user);
+            System.out.println("user added");
+            UserDataSet.save(user2);
+            System.out.println("user added");
+
+            UserDataSet loadedUser = UserDataSet.load(2, UserDataSet.class);
+            System.out.println("user loaded: " + loadedUser.getName() + ", " + loadedUser.getAge());
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void executorNoResult(Statement s, String query) throws SQLException {
+        s.executeUpdate(query);
+    }
+}
