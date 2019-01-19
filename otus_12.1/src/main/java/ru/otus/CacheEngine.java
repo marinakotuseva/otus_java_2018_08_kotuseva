d42f4cbc00638bb835be429292870840c40a2470
Marina <avesutok@gmail.com>
2019-01-19 21:29:51 +0100

    Spring bean

---
+++ otus_12.1/src/main/java/ru/otus/CacheEngine.java
@@ -0,0 +1,71 @@
+package ru.otus;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+
+public class CacheEngine<K, V> {
+    private final int maxElements;
+    private final long lifeTimeMs;
+
+    private final HashMap<K, SoftReference<MapElement<K, V>>> elements = new HashMap<K, SoftReference<MapElement<K, V>>>();
+
+    CacheEngine(int m, long l) {
+        this.maxElements = m;
+        this.lifeTimeMs = l > 0 ? l : 0;
+    }
+
+    public void add(MapElement<K,V> element){
+        if (elements.size() == maxElements){
+            K firstKey = elements.keySet().iterator().next();
+            System.out.println(firstKey);
+            System.out.println("->> Deleting from cache element " + elements.get(firstKey) + " due to size exceeding");
+            elements.remove(firstKey);
+        }
+        elements.put(element.getKey(), new SoftReference<>(element));
+        System.out.println("Added to cache " + element.getKey() + " " + element.getValue());
+
+    }
+
+    public V get(K key){
+        SoftReference<MapElement<K, V>> element = elements.get(key);
+        if (element == null) {
+            return null;
+        } else {
+            return element.get().getValue();
+        }
+    }
+
+    public int size(){
+        return elements.size();
+    }
+}
+
+
+class MapElement<K,V>{
+    private final K key;
+    private final V value;
+
+    MapElement(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+    public V getValue() {
+        return value;
+    }
+}
+
+
+class BigObject {
+    final byte[] array = new byte[1024 * 1024];
+
+    public byte[] getArray() {
+        return array;
+    }
+}
--- otus_12.1/src/main/java/ru/otus/Main.java
+++ otus_12.1/src/main/java/ru/otus/Main.java
@@ -2,10 +2,24 @@ package ru.otus;
 
 
 import org.eclipse.jetty.server.Server;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        int addToCacheSize = 13;
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("SpringBeans.xml");
+        CacheEngine<Integer, String> cache = context.getBean("cacheEngine", CacheEngine.class);
+        System.out.println("Created cache with " + cache.size() + " elements");
+
+        for (int i =0; i< addToCacheSize; i++){
+            cache.add( new MapElement<>(i, new String("Hello, world!")));
+        }
+        System.out.println("Added to cache " + addToCacheSize + " elements");
+
 
         ServerHelper serverHelper = new ServerHelper();
         Server server = serverHelper.setServer();
